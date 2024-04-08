6bb585f48cdbd5f1b35452d47ea81ef0939bff38
Nhokcoilc2k1 <hoanghungbhbylc@gmail.com>
2024-04-08 14:27:10 +0700

    Loi phan register

---
+++ src/services/auth.js
@@ -0,0 +1,42 @@
+import db from "../models";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { v4 } from "uuid";
+require("dotenv").config();
+
+const hashPassword = (password) =>
+  bcrypt.hashSync(password, bcrypt.genSaltSync(12));
+
+export const registerService = ({ name, phone, password }) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const response = await db.User.findOrCreate({
+        // response sẽ nhận 1 mảng 2 phần tử. phần tử đầu trả về true || false, phần tử 2 trả về DL
+        where: { phone },
+        defaults: {
+          phone,
+          name,
+          password: hashPassword(password),
+          id: v4(),
+        },
+      });
+
+      const token =
+        response[1] &&
+        jwt.sign(
+          { id: response[0].id, phone: response[0].phone },
+          process.env.SECRET_KEY,
+          { expiresIn: "2d" }
+        );
+
+      resolve({
+        err: token ? 0 : 2,
+        msg: token
+          ? "Register is successfully !"
+          : "This phone number already exists !",
+        token: token || null,
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
