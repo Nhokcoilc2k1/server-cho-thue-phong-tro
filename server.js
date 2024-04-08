08f70a191f994ee0e9e5c72a3048dba3dcd1fba5
Nhokcoilc2k1 <hoanghungbhbylc@gmail.com>
2024-04-08 08:28:44 +0700

    Tao base server nodeJS

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import initRoutes from "./src/routes";
+
+dotenv.config();
+
+const app = express();
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["POST", "GET", "PUT", "DELETE"],
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use("/", (req, res) => {
+//   res.send("server on ...");
+// });
+initRoutes(app);
+
+const port = process.env.PORT || 8888;
+const listener = app.listen(port, () => {
+  console.log(`Server is running on the port ${listener.address().port}`);
+});
